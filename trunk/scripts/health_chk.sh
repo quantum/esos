ea51ba3066dcc0201ffaccb634fb42024f6d92ab
msmith626@gmail.com <msmith626@gmail.com@4ef4c33c-8b6d-1c94-9bb4-a719c1286391>
2012-03-04 19:40:41 +0000

    implemented check for AACRAID logical drives

--- trunk/scripts/health_chk.sh
+++ trunk/scripts/health_chk.sh
@@ -12,10 +12,12 @@ ARCCONF="/opt/sbin/arcconf"
 # Check MegaRAID logical drives (if any)
 if [ -x "${MEGACLI}" ]; then
 	# Get the number of adapters on the system
+	# Adapter numbers start with 0 for MegaRAID
 	adp_count=`${MEGACLI} -adpCount | grep "Controller Count:" | \
 		cut -d: -f2 | tr -d ' ' | tr -d '.' | tr -d '\n'`
 	for adapter in `seq 0 $(expr ${adp_count} - 1)`; do
 		# Get the number of logical drives for the adapter
+		# Logical drive numbers start with 0 for MegaRAID
 		ld_count=`${MEGACLI} -AdpAllInfo -a${adapter} | \
 			grep "Virtual Drives    :" | cut -d: -f2 | tr -d ' ' | tr -d '\n'`
 		for logical_drv in `seq 0 $(expr ${ld_count} - 1)`; do
@@ -28,3 +30,29 @@ if [ -x "${MEGACLI}" ]; then
 		done
 	done
 fi
+
+# Check AACRAID logical drives (if any)
+if [ -x "${ARCCONF}" ]; then
+	# Get the number of adapters on the system
+	# Adapter numbers start with 1 for AACRAID
+	adp_count=`${ARCCONF} GETVERSION nologs | grep "Controllers found:" | \
+		cut -d: -f2 | tr -d ' ' | tr -d '\n'`
+	for adapter in `seq 1 ${adp_count}`; do
+		# Get the number of logical drives for the adapter
+		# Logical drive numbers start with 0 for AACRAID
+		ld_count=`${ARCCONF} GETCONFIG ${adapter} AD nologs | \
+			grep "Logical devices/Failed/Degraded          :" | \
+			cut -d: -f2 | cut -d/ -f1 | tr -d ' ' | tr -d '\n'`
+		for logical_drv in `seq 0 $(expr ${ld_count} - 1)`; do
+			ld_state=`${ARCCONF} GETCONFIG ${adapter} LD ${logical_drv} nologs | \
+				grep "Status of logical device                 :" |  -f2 | \
+				tr -d ' ' | tr -d '\n'`
+			if [ "${ld_state}" != "Optimal" ]; then
+				echo "** Warning! AACRAID logical drive ${logical_drv} on adapter ${adapter} is not optimal!" 1>&2
+				${ARCCONF} GETCONFIG ${adapter} LD ${logical_drv} nologs 1>&2
+			fi
+		done
+	done
+fi
+
+
