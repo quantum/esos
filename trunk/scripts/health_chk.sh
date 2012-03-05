2d876c4e60e01a839a4dacf2feca8babab50b8be
msmith626@gmail.com <msmith626@gmail.com@4ef4c33c-8b6d-1c94-9bb4-a719c1286391>
2012-03-05 03:23:06 +0000

    fixed a few issues in health_chk, added health_chk to cron and Makefile.in

--- trunk/scripts/health_chk.sh
+++ trunk/scripts/health_chk.sh
@@ -16,21 +16,21 @@ if [ -x "${MEGACLI}" ]; then
 	echo "Starting MegaRAID logical drive checks..."
 	# Get the number of adapters on the system
 	# Adapter numbers start with 0 for MegaRAID
-	adp_count=`${MEGACLI} -adpCount | grep "Controller Count:" | \
+	adp_count=`${MEGACLI} -adpCount -NoLog | grep "Controller Count:" | \
 		cut -d: -f2 | tr -d ' ' | tr -d '.' | tr -d '\n'`
 	echo "Number of adapters found: ${adp_count}"
 	for adapter in `seq 0 $(expr ${adp_count} - 1)`; do
 		# Get the number of logical drives for the adapter
 		# Logical drive numbers start with 0 for MegaRAID
-		ld_count=`${MEGACLI} -AdpAllInfo -a${adapter} | \
+		ld_count=`${MEGACLI} -AdpAllInfo -a${adapter} -NoLog | \
 			grep "Virtual Drives    :" | cut -d: -f2 | tr -d ' ' | tr -d '\n'`
 		echo "Adapter ${adapter} has ${ld_count} logical drive(s)."
 		for logical_drv in `seq 0 $(expr ${ld_count} - 1)`; do
-			ld_state=`${MEGACLI} -LDInfo -L${logical_drv} -a${adapter} | \
+			ld_state=`${MEGACLI} -LDInfo -L${logical_drv} -a${adapter} -NoLog | \
 				grep "State               :" | cut -d: -f2 | tr -d ' ' | tr -d '\n'`
 			if [ "${ld_state}" != "Optimal" ]; then
 				echo "** Warning! MegaRAID logical drive ${logical_drv} on adapter ${adapter} is not optimal!" 1>&2
-				${MEGACLI} -LDInfo -L${logical_drv} -a${adapter} 1>&2
+				${MEGACLI} -LDInfo -L${logical_drv} -a${adapter} -NoLog 1>&2
 			fi
 		done
 	done
@@ -75,7 +75,7 @@ mem_used=`free -m | grep "Mem:" | awk '{print $3}'`
 mem_free=`free -m | grep "Mem:" | awk '{print $4}'`
 echo "Physical RAM check..."
 echo -e "Total Memory:\t${mem_total} MB\nUsed Memory:\t${mem_used} MB\nFree Memory:\t${mem_free} MB"
-prct_mem_used=`echo "${mem_used} ${mem_total}" | awk 'BEGIN { printf("%.1g", $1 / $2) }'`
+prct_mem_used=`echo "${mem_used} ${mem_total}" | awk '{ printf("%.1g", $1 / $2) }'`
 echo "Memory used percent: ${prct_mem_used}"
 if expr ${prct_mem_used} '>' ${MEM_PRCT_THRESH} > /dev/null; then
 	echo "** Warning! Maximum memory used threshold (${MEM_PRCT_THRESH}) has been exceeded..." 1>&2
@@ -89,7 +89,7 @@ disk_used=`df -m / | grep tmpfs | awk '{print $3}'`
 disk_avail=`df -m / | grep tmpfs | awk '{print $4}'`
 echo "Disk (/ -> root tmpfs) space check..."
 echo -e "Total Disk Space:\t${disk_total} MB\nUsed Disk Space:\t${disk_used} MB\nAvail. Disk Space:\t${disk_avail} MB"
-prct_disk_used=`echo "${disk_used} ${disk_total}" | awk 'BEGIN { printf("%.1g", $1 / $2) }'`
+prct_disk_used=`echo "${disk_used} ${disk_total}" | awk '{ printf("%.1g", $1 / $2) }'`
 echo "Disk space used percent: ${prct_disk_used}"
 if expr ${prct_disk_used} '>' ${DISK_PRCT_THRESH} > /dev/null; then
 	echo "** Warning! Maximum disk space used threshold (${DISK_PRCT_THRESH}) has been exceeded..." 1>&2
