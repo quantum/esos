4c17b817366f4765d38148b73d8b1959d64d691e
Marc A. Smith <msmith626@gmail.com>
2012-06-13 20:53:33 +0000

    implemented device node unique ID for SCSI disks in /dev (via mdev)

---
+++ scripts/dev_nodes.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# $Id$
+
+# This script is called by mdev and attempts to create a link to
+# the SCSI disk using the ID via the sg_vpd utility. If sg_vpd
+# fails (eg, no VPD page for the device) then do nothing.
+
+scsi_id=`sg_vpd -i -q /dev/${MDEV} 2>&1`
+if [ $? = "0" ]; then
+	mkdir -p /dev/disk-by-id
+	ln -s /dev/${MDEV} /dev/disk-by-id/${scsi_id}
+fi
