9a0da872a8a0fbff8312a00dfdd4bed467383827
Marc A. Smith <msmith626@gmail.com>
2013-02-11 03:02:55 +0000

    set --jobs=4 for Linux kernel and glibc packages to hopefully speed up build time; start system logger after network is up (after hostname has been set); move temp. directory creation to after image has been written in the install script; re-read the partition table after the disk has been written... should fix issue that some distros experience where FS by label doesn't work; fixed issue with mdev device node helper script where sg_vpd tool was not returning non-zero and giving bad results

--- scripts/dev_nodes.sh
+++ scripts/dev_nodes.sh
@@ -6,8 +6,11 @@
 # the SCSI disk using the ID via the sg_vpd utility. If sg_vpd
 # fails (eg, no VPD page for the device) then do nothing.
 
+# Check the first two chracters of the 'scsi_id' variable; the
+# sg_vpd utility may return 0 even when it doesn't get a proper ID.
+
 scsi_id=`sg_vpd -i -q /dev/${MDEV} 2>&1`
-if [ $? = "0" ]; then
+if [ ${?} -eq 0 ] && [ "${scsi_id:0:2}" == "0x" ]; then
 	mkdir -m 0755 -p /dev/disk-by-id
 	ln -s /dev/${MDEV} /dev/disk-by-id/${scsi_id}
 fi
