a3bd3aedf6eafceab3d3a5dec089fdd74b91c7b0
Marc A. Smith <msmith626@gmail.com>
2014-03-31 20:11:44 +0000

    use ELF output with makedumpfile tool (initramfs init); started moving more exotic strings into new strings.c source file; updated autoconf script to make /tmp in initramfs stub (required by makedumpfile)

---
+++ tui/strings.c
@@ -0,0 +1,17 @@
+/*
+ * $Id$
+ */
+
+/* Dialog radio widget options */
+char *g_no_yes_opts[] = {"No", "Yes"},
+        *g_auth_meth_opts[] = {"None", "Plain Text", "CRAM-MD5"},
+        *g_ip_opts[] = {"Disabled", "Static", "DHCP"};
+
+/* Label title strings */
+char *g_mail_title_msg[] = {"</31/B>System mail (SMTP) settings..."},
+        *g_add_user_title_msg[] = {"</31/B>Adding a new ESOS user account..."},
+        *g_date_title_msg[] = {"</31/B>Edit date and time settings..."};
+
+/* Misc. widget related strings */
+char *g_choice_char[] = {"[ ] ", "[*] "},
+        *g_bonding_map[] = {"None", "Master", "Slave"};
