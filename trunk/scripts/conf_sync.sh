b32696e78b2c3e588f2761f1880334372232c610
msmith626@gmail.com <msmith626@gmail.com@4ef4c33c-8b6d-1c94-9bb4-a719c1286391>
2014-09-06 02:17:41 +0000

    resolved issue in esos::scst RA where the pre-scst-xtra conf file would not be executed; updated TUI code to handle btrfs file system; resolved issue # 87 (usb_sync.sh was not called if SCST wasn't loaded); added btrfs-progs and deps (attr, acl); enabled btrfs file system support in kernel config; updated Makefile to handle fine grain cleaning of .la files (fixes rare build issue)

--- trunk/scripts/conf_sync.sh
+++ trunk/scripts/conf_sync.sh
@@ -3,6 +3,6 @@
 # $Id$
 
 # Write the SCST configuration to a file (we don't hide stderr)
-/usr/sbin/scstadmin -force -nonkey -write_config /etc/scst.conf > /dev/null || exit 1
+/usr/sbin/scstadmin -force -nonkey -write_config /etc/scst.conf > /dev/null
 # Synchronize the local configuration with the USB flash drive
 /usr/local/sbin/usb_sync.sh || exit 1
