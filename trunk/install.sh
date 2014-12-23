5fa67661d6d0f0f281e317ae760ce513d91a2f15
msmith626@gmail.com <msmith626@gmail.com@4ef4c33c-8b6d-1c94-9bb4-a719c1286391>
2014-12-23 14:21:57 +0000

    updates to Makefile and installer scripts to compress the image file with bzip2 and handle it when installing, allowing hosts with < 4 GB of usable RAM to do an in-place upgrade; new local rpm2cpio.sh shell script which replaces the rpm2cpio bin function and eliminates the dependency; modified the Windows installer script to handle USB drives that appear as "fixed disk" appropriately; the install.sh script now supports Mac OS X as an install platform

--- trunk/install.sh
+++ trunk/install.sh
@@ -5,23 +5,38 @@
 PKG_DIR="${PWD}"
 TEMP_DIR="${PKG_DIR}/temp"
 MNT_DIR="${TEMP_DIR}/mnt"
-REQD_TOOLS="tar rpm2cpio cpio dd md5sum sha256sum grep blockdev unzip findfs"
+RPM2CPIO="${PKG_DIR}/rpm2cpio.sh"
+LINUX_REQD_TOOLS="tar cpio dd md5sum sha256sum grep blockdev lsscsi unzip findfs bunzip2"
+MACOSX_REQD_TOOLS="tar cpio dd md5 shasum cat cut sed diff grep diskutil unzip bunzip2 fuse-ext2"
 MD5_CHECKSUM="dist_md5sum.txt"
 SHA256_CHECKSUM="dist_sha256sum.txt"
+LINUX="LINUX"
+MACOSX="MACOSX"
 
 source ./install_common
 
 echo "*** Enterprise Storage OS Install Script ***" && echo
 
 # Need root privileges for installer
-if [ `whoami` != "root" ]; then
+if [ "x$(whoami)" != "xroot" ]; then
     echo "### This installer requires root privileges."
+    exit 1
+fi
+
+# What operating system is this
+if [ "x$(uname -s)" = "xLinux" ]; then
+    this_os="${LINUX}"
+elif [ "x$(uname -s)" = "xDarwin" ]; then
+    this_os="${MACOSX}"
+else
+    echo "ERROR: Only Linux and Mac OS X are supported with this script!"
     exit 1
 fi
 
 # Make sure the required tools/utilities are available
 echo "### Checking for required tools..."
-for i in ${REQD_TOOLS}; do
+reqd_tools=${this_os}_REQD_TOOLS
+for i in ${!reqd_tools}; do
     if ! which ${i} > /dev/null 2>&1; then
         echo "ERROR: The '${i}' utility is required to use this installation script."
         exit 1
@@ -31,16 +46,29 @@ echo
 
 # Checksums
 echo "### Verifying checksums..."
-md5sum -w -c ${MD5_CHECKSUM} || exit 1
-sha256sum -w -c ${SHA256_CHECKSUM} || exit 1
+if [ "${this_os}" = "${LINUX}" ]; then
+    md5sum -w -c ${MD5_CHECKSUM} || exit 1
+    sha256sum -w -c ${SHA256_CHECKSUM} || exit 1
+elif [ "${this_os}" = "${MACOSX}" ]; then
+    img_file="$(cat ${MD5_CHECKSUM} | cut -d' ' -f2 | sed -e 's/\*//')"
+    if md5 -r ${img_file} | sed 's/ / */' | diff ${MD5_CHECKSUM} - > /dev/null 2>&1; then
+        echo "${img_file}: OK"
+    else
+        echo "${img_file}: The MD5 checksum doesn't match!"
+        exit 1
+    fi
+    shasum -a 256 -c ${SHA256_CHECKSUM} || exit 1
+fi
 echo
 
-# If the lsscsi program is available, list SCSI devices
-if which lsscsi > /dev/null 2>&1; then
-    echo "### Here is a list of SCSI devices on this machine:"
+# Print out a list of disk devices
+echo "### Here is a list of disk devices on this machine:"
+if [ "${this_os}" = "${LINUX}" ]; then
     lsscsi
-    echo
+elif [ "${this_os}" = "${MACOSX}" ]; then
+    diskutil list
 fi
+echo
 
 # Get desired USB drive device node and perform a few checks
 echo "### Please type the full path of your USB drive device node (eg, /dev/sdz):" && read dev_node
@@ -49,12 +77,17 @@ if [ "${dev_node}" = "" ] || [ ! -e ${dev_node} ]; then
     echo "ERROR: That device node doesn't seem to exist."
     exit 1
 fi
-if grep ${dev_node} /proc/mounts > /dev/null; then
+if mount | grep ${dev_node} > /dev/null; then
     echo "ERROR: It looks like that device is mounted; unmount it and try again."
     exit 1
 fi
-dev_sectors=`blockdev --getsz ${dev_node}` || exit 1
-dev_bytes=`expr ${dev_sectors} \\* 512`
+if [ "${this_os}" = "${LINUX}" ]; then
+    dev_sectors=`blockdev --getsz ${dev_node}` || exit 1
+    dev_bytes=`expr ${dev_sectors} \\* 512`
+elif [ "${this_os}" = "${MACOSX}" ]; then
+    dev_bytes=$(diskutil info -plist ${dev_node} | grep -A1 "<key>TotalSize</key>" | \
+        grep -o '<integer>'.*'</integer>' | grep -o [^'<'integer'>'].*[^'<''/'integer'>']) || exit 1
+fi
 if [ ${dev_bytes} -lt 4000000000 ]; then
     echo "ERROR: Your USB flash drive isn't large enough; it must be at least 4000 MiB."
     exit 1
@@ -64,10 +97,12 @@ fi
 echo "### Proceeding will completely wipe the '${dev_node}' device. Are you sure?" && read confirm
 echo
 if [ "${confirm}" = "yes" ] || [ "${confirm}" = "y" ]; then
-    image_file=`ls esos-*.img`
+    image_file=`ls esos-*.img.bz2`
     echo "### Writing ${image_file} to ${dev_node}; this may take a while..."
-    dd if=./${image_file} of=${dev_node} bs=1M || exit 1
-    blockdev --rereadpt ${dev_node} || exit 1
+    bunzip2 -d -c ${image_file} | dd of=${dev_node} bs=1m || exit 1
+    if [ "${this_os}" = "${LINUX}" ]; then
+        blockdev --rereadpt ${dev_node} || exit 1
+    fi
     echo
     echo "### It appears the image was successfully written to disk (no errors reported)!"
 else
@@ -117,30 +152,37 @@ if [ -z "${install_list}" ]; then
 else
     echo "### Installing proprietary CLI tools..."
     mkdir -p ${MNT_DIR} || exit 1
-    esos_conf=$(findfs LABEL=esos_conf)
-    if [ ${?} -ne 0 ]; then
-        exit 1
-    fi
-    # Just in case this system has an auto-mounter
-    if grep ${esos_conf} /proc/mounts > /dev/null 2>&1; then
-        umount ${esos_conf} || exit 1
+    if [ "${this_os}" = "${LINUX}" ]; then
+        esos_conf=$(findfs LABEL=esos_conf)
+        if [ ${?} -ne 0 ]; then
+            exit 1
+        fi
+        # Just in case this system has an auto-mounter
+        if grep ${esos_conf} /proc/mounts > /dev/null 2>&1; then
+            umount ${esos_conf} || exit 1
+        fi
     fi
     # Mount it and inject all (if any) of the tools
-    mount ${esos_conf} ${MNT_DIR} || exit 1
+    if [ "${this_os}" = "${LINUX}" ]; then
+        mount ${esos_conf} ${MNT_DIR} || exit 1
+    elif [ "${this_os}" = "${MACOSX}" ]; then
+        fuse-ext2 -o force ${dev_node}s3 ${MNT_DIR} || exit 1
+        sleep 5
+    fi
     mkdir -p ${MNT_DIR}/opt/{sbin,lib} || exit 1
     cd ${TEMP_DIR}
     for i in ${install_list}; do
         if [ "${i}" = "MegaCLI" ]; then
-            unzip -o ${PKG_DIR}/*_MegaCLI.zip && rpm2cpio Linux/MegaCli-*.rpm | \
+            unzip -o ${PKG_DIR}/*_MegaCLI.zip && ${RPM2CPIO} Linux/MegaCli-*.rpm | \
             cpio -idmv && cp opt/MegaRAID/MegaCli/MegaCli64 ${MNT_DIR}/opt/sbin/
         elif [ "${i}" = "StorCLI" ]; then
-            unzip -o ${PKG_DIR}/*_StorCLI.zip && rpm2cpio storcli_all_os/Linux/storcli-*.rpm | \
+            unzip -o ${PKG_DIR}/*_StorCLI.zip && ${RPM2CPIO} storcli_all_os/Linux/storcli-*.rpm | \
             cpio -idmv && cp opt/MegaRAID/storcli/storcli64 ${MNT_DIR}/opt/sbin/ && \
             cp opt/MegaRAID/storcli/libstorelibir* ${MNT_DIR}/opt/lib/
         elif [ "${i}" = "arcconf" ]; then
             unzip -o ${PKG_DIR}/arcconf_*.zip && cp linux_x64/arcconf ${MNT_DIR}/opt/sbin/
         elif [ "${i}" = "hpacucli" ]; then
-            rpm2cpio ${PKG_DIR}/hpacucli-*.x86_64.rpm | cpio -idmv && \
+            ${RPM2CPIO} ${PKG_DIR}/hpacucli-*.x86_64.rpm | cpio -idmv && \
             cp opt/compaq/hpacucli/bld/.hpacucli ${MNT_DIR}/opt/sbin/hpacucli && \
             cp opt/compaq/hpacucli/bld/*.so ${MNT_DIR}/opt/lib/
         elif [ "${i}" = "linuxcli" ]; then
