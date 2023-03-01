49d4a0e4263e72e92e66f4f848e8e89e70a401f2
Marc A. Smith <marc.smith@quantum.com>
2023-03-01 09:56:53 -0500

    rc.libvirtdexporter: updates for the new pwatcher.sh arguments / handling; pwatcher.sh: handling touching the lock file instead of in rc script, and support an additional (optional) status test that can be done after checking for the PID

--- scripts/pwatcher.sh
+++ scripts/pwatcher.sh
@@ -1,75 +1,101 @@
-#! /bin/sh
+#! /bin/bash
 
-#shellcheck disable=SC3046,1091
+# shellcheck disable=SC1091
 source /etc/rc.d/common
 
 WATCHER_LOG="/var/log/pwatcher.log"
 WATCH_PROCESS_ARGS=""
 
-stop() {
-    /bin/echo "$(date "+%F %T %Z") Stopping ${WATCH_PROCESS}..." >> $WATCHER_LOG
-    watch_pid=$(/usr/bin/pidof "${WATCH_PROCESS}")
-    if [ -n "$watch_pid" ]; then
-        /usr/bin/kill -TERM "${watch_pid}" || exit 1
-        wait_for_stop "${WATCH_PROCESS}"
-    fi
-    exit
+log_it() {
+    echo "$(date "+%F %T %Z") ${*}" >> "${WATCHER_LOG}"
 }
 
 start() {
-    /bin/echo "$(date "+%F %T %Z") Starting ${WATCH_PROCESS}..." >> $WATCHER_LOG
+    log_it "Starting '${WATCH_PROCESS}'..."
+    log_it "STATUS_TEST='${STATUS_TEST}'"
+    log_it "LOCK_FILE='${LOCK_FILE}'"
+    log_it "WATCH_PROCESS='${WATCH_PROCESS}'"
+    log_it "WATCH_PROCESS_ARGS='${WATCH_PROCESS_ARGS}'"
     # shellcheck disable=SC2086
     ${WATCH_PROCESS} ${WATCH_PROCESS_ARGS} > /dev/null 2>&1 &
+    touch "${LOCK_FILE}"
+}
+
+stop() {
+    log_it "Stopping '${WATCH_PROCESS}'..."
+    watch_pid=$(pidof -x "${WATCH_PROCESS}")
+    if [ -n "${watch_pid}" ]; then
+        kill -TERM "${watch_pid}" || exit 1
+        wait_for_stop "${WATCH_PROCESS}"
+    fi
 }
 
 status() {
     # Check for either a matching running daemon process or a subprocess
     # running the process
-    /usr/bin/pidof "${WATCH_PROCESS}" > /dev/null 2>&1
-    RC=$?
-    /usr/bin/pidof -x "${WATCH_PROCESS}" > /dev/null 2>&1
-    RC2=$?
-    if [ $RC -ne 0 ] && [ $RC2 -ne 0 ]; then
+    pidof "${WATCH_PROCESS}" > /dev/null 2>&1
+    rc=${?}
+    pidof -x "${WATCH_PROCESS}" > /dev/null 2>&1
+    rc2=${?}
+    if [ ${rc} -ne 0 ] && [ ${rc2} -ne 0 ]; then
+        # Only log the message if we have a lock file
+        if [ -e "${LOCK_FILE}" ]; then
+            log_it "No PID detected for the '${WATCH_PROCESS}' process!" \
+                "It must have died unexpectedly..."
+        fi
         return 1
+    elif [ -n "${STATUS_TEST}" ]; then
+        if ! eval "${STATUS_TEST}"; then
+            log_it "The additional status test '${STATUS_TEST}' exited" \
+                "non-zero for the '${WATCH_PROCESS}' process!" \
+                "Attempting to stop it now..."
+            stop
+            return 1
+        fi
     else
         return 0
     fi
 }
 
-# stop the process that this watcher started on exit
+# Stop the process that this watcher started on exit
 trap "stop; exit" INT TERM QUIT
 
+# Parse the arguments
+STATUS_TEST="${1}"
+shift
+LOCK_FILE="${1}"
+shift
 WATCH_PROCESS="${1}"
-if [ -z "${WATCH_PROCESS}" ]; then
-    echo "Usage: $0 process_name [process_args]"
+shift
+if [ -z "${LOCK_FILE}" ] || [ -z "${WATCH_PROCESS}" ]; then
+    echo "Usage: ${0} STATUS_TEST LOCK_FILE PROCESS_NAME [PROCESS_ARGS]"
     exit 1
 fi
-
-shift
-#shellcheck disable=SC2124
+# shellcheck disable=SC2124
 WATCH_PROCESS_ARGS="${@}"
 
 # Look at all pwatcher processes and see if we have one already
-# running for the requested $WATCH_PROCESS
-existing_pids=$(/usr/bin/pidof -x -o %PPID "$0")
-# shellcheck disable=SC3011
-if [ "$(wc -w <<< "$existing_pids")" -ge 1 ]; then
-    for existing_pid in $existing_pids
-    do
-        if grep -q "$WATCH_PROCESS" "/proc/${existing_pid}/cmdline"; then
-            echo "Already have a pwatcher process running for ${WATCH_PROCESS}, exiting"
+# running for the requested WATCH_PROCESS
+existing_pids="$(pidof -x -o %PPID "${0}")"
+if [ "$(wc -w <<< "${existing_pids}")" -ge 1 ]; then
+    for existing_pid in ${existing_pids}; do
+        if grep -q "${WATCH_PROCESS}" "/proc/${existing_pid}/cmdline"; then
+            echo "An existing process watcher running for '${WATCH_PROCESS}'" \
+                "was detected, exiting..."
             exit 0
         fi
     done
 fi
 
-while true
-do
+# We loop here indefinitely, monitoring the watched process
+while true; do
     if ! status; then
-        /bin/echo "$(date "+%F %T %Z") ${WATCH_PROCESS} process not running, starting" >> $WATCHER_LOG
+        log_it "The '${WATCH_PROCESS}' process is not running," \
+            "attempting to start it..."
         start
         sleep 5
     else
         sleep 5
     fi
 done
+
